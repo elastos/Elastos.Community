a9639df1f89961a153a9612bf95e8bd1fa413e68
erlichchen <chenxuewei.dev@gmail.com>
2018-07-21 12:13:06 -0700

    translate i18N file for chinese

--- ElastosBountyProgram/front-end/src/I18N/zh.js
+++ ElastosBountyProgram/front-end/src/I18N/zh.js
@@ -1,3 +1,52 @@
 export default {
+    // Header
+    '0001': '赏金计划',    // Bounty Programs
+    '0002': '社区',       // Community
+    '0003': '主办单位',    // Organizers
+    '0004': '帐户',       // Account
+    '0005': '小组',       // Teams
+    '0006': '任务',       // Tasks
+    '0007': '如何赚取ELA', // How to Earn ELA
+    '0008': '关于',       // About
+    '0009': '常问问题',    // FAQ
+    '0010': '联系方式',    // Contact
+    '0011': '加入Slack',  // Join Slack
 
+    '0100': '开发者', // Developers
+    '0101': '活动',   // Events
+    '0102': '社区',   // Community
+
+    '0200': '简介',   // Profile
+    '0201': '登录',   // Login
+    '0202': '注册',   // Register
+    '0203': '管理员', // Admin
+    '0204': '登出',   // Logout
+
+    // Admin breadcrumb
+    '1100': '管理员',  // Admin    
+
+    // Admin users
+    '1200': '没有',     // No      
+    '1201': '用户名',   // Username
+    '1202': '电子邮件', // Email
+    '1203': '默认语言', // Default language
+    '1204': '职能',    // Role 
+    '1205': '活跃',    // Active
+    '1206': 'Ela',    // Ela
+    '1207': '创建日期', // Created date
+
+    // Admin navigator
+    '1300': '任务', // Tasks
+    '1301': '社区', // Community
+    '1302': '用户', // Users
+    '1303': '小组', // Teams
+    '1304': '提交', //Submissions
+    '1305': '表格', // Forms
+
+    // Profile navigator
+    '2300': '信息', // Info
+    '2301': '任务', // Tasks
+    '2302': '小组', // Teams
+    '2303': '提交', // Submissions
+    '2304': '社区', //Communities
 };
