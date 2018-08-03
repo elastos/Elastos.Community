ab3a2cea75b5300269cb858e2fbfe317002ff54e
Wiktor Starczewski <wiktor@squirro.com>
2018-08-03 19:12:25 +0200

    Wire up the reducer to the team service

--- ElastosBountyProgram/front-end/src/service/TeamService.js
+++ ElastosBountyProgram/front-end/src/service/TeamService.js
@@ -4,21 +4,41 @@ import {api_request} from '@/util'
 
 export default class extends BaseService {
 
-    async list(filter={}){
+    async list(filter = {}) {
         const result = await api_request({
             path: '/api/team/list',
             method: 'get',
             data: filter
         });
 
-        return result;
+        return result
+    }
+
+    async index(qry) {
+        const teamRedux = this.store.getRedux('team')
+
+        this.dispatch(teamRedux.actions.loading_update(true))
+
+        const result = await api_request({
+            path: '/api/team/list',
+            method: 'get',
+            data: qry
+        })
+
+        this.dispatch(teamRedux.actions.loading_update(false))
+        this.dispatch(teamRedux.actions.all_teams_reset())
+        this.dispatch(teamRedux.actions.all_teams_update(result.list))
+
+        return result
     }
 
     async getUserTeams(userId) {
-
         const userRedux = this.store.getRedux('user')
+        const teamRedux = this.store.getRedux('team')
 
         this.dispatch(userRedux.actions.loading_update(true))
+        this.dispatch(teamRedux.actions.loading_update(true))
+
         const result = await api_request({
             path: '/api/team/list',
             method: 'get',
@@ -29,38 +49,50 @@ export default class extends BaseService {
 
         this.dispatch(userRedux.actions.loading_update(false))
         this.dispatch(userRedux.actions.teams_update(result.list))
+        this.dispatch(teamRedux.actions.loading_update(false))
+        this.dispatch(teamRedux.actions.all_teams_reset())
+        this.dispatch(teamRedux.actions.all_teams_update(result.list))
 
         return result
     }
 
-    async getDetail(teamId){
+    async getDetail(teamId) {
+        const teamRedux = this.store.getRedux('team')
+        this.dispatch(teamRedux.actions.loading_update(true))
+
         const result = await api_request({
-            path : '/api/team/get',
-            method : 'get',
-            data : {
-                teamId,
-                status : 'NORMAL'
+            path: '/api/team/get',
+            method: 'get',
+            data: {
+                teamId
             }
-        });
+        })
 
-        return result;
+        this.dispatch(teamRedux.actions.loading_update(false))
+
+        return result
     }
 
-    async update(param){
+    async update(param) {
+        const teamRedux = this.store.getRedux('team')
+        this.dispatch(teamRedux.actions.loading_update(true))
+
         const result = await api_request({
-            path : '/api/team/update',
-            method : 'post',
-            data : param
+            path: '/api/team/update',
+            method: 'post',
+            data: param
         });
 
-        return result;
+        this.dispatch(teamRedux.actions.loading_update(false))
+
+        return result
     }
 
-    async create(param){
+    async create(param) {
         const result = await api_request({
-            path : '/api/team/create',
-            method : 'post',
-            data : param
+            path: '/api/team/create',
+            method: 'post',
+            data: param
         });
 
         return result;
