e1134fac35971a23751ccaa9f1f369ad3b6832e9
Semir Ramovic <semiir.ramovic@gmail.com>
2018-08-07 22:43:45 +0200

    Developer Search sidebar v1

--- ElastosBountyProgram/front-end/src/module/page/developer/search/Component.js
+++ ElastosBountyProgram/front-end/src/module/page/developer/search/Component.js
@@ -5,11 +5,21 @@ import config from '@/config'
 import _ from 'lodash'
 import './style.scss'
 
-import { Col, Row, Icon, Form, Input, Button, Breadcrumb, List, Avatar } from 'antd'
+import { Col, Row, Icon, Input, Button, Breadcrumb, List, Checkbox, Radio } from 'antd'
 import moment from 'moment/moment'
+
+const Search = Input.Search;
+const CheckboxGroup = Checkbox.Group;
+const RadioGroup = Radio.Group;
 
 export default class extends StandardPage {
     state = {
+        lookingFor: 0,
+        skillset: [0],
+        category: [0],
+        entryCount: 4,
+        skillsetShowAllEntries: false,
+        categoryShowAllEntries: false,
     }
 
     componentDidMount() {
@@ -21,7 +31,94 @@ export default class extends StandardPage {
         this.props.resetTeams()
     }
 
+    onChangeLookingFor(e) {
+        this.setState({
+            lookingFor: e.target.value
+        });
+    }
+
+    onChangeSkillset(value) {
+        // [0,1 ... n]
+    }
+
+    onChangeCategory() {
+        // [0,1 ... n]
+    }
+
+    renderLookingFor(lookingForOptions) {
+        let elements = [];
+        for(let i = 0; i < lookingForOptions.length
+            && (i < this.state.entryCount || this.state.skillsetShowAllEntries); i++) {
+            elements.push(<Radio className="radio" value={i} key={i}>{lookingForOptions[i]}</Radio>);
+        }
+
+        return(
+            <RadioGroup onChange={this.onChangeLookingFor.bind(this)} value={this.state.lookingFor}>
+                {elements}
+            </RadioGroup>
+        );
+    }
+
+    renderSkillset(skillsetOptions) {
+        let elements = [];
+        for(let i = 0; i < skillsetOptions.length
+            && ( i < this.state.entryCount || this.state.skillsetShowAllEntries); i++) {
+            elements.push(
+                <div className="checkbox" key={i}>
+                    <Checkbox value={i} key={i}>
+                        {skillsetOptions[i]}
+                    </Checkbox>
+                </div>
+            );
+        }
+
+        return(
+            <CheckboxGroup onChange={this.onChangeSkillset.bind(this)}>
+                {elements}
+            </CheckboxGroup>
+        );
+    }
+
+    renderCategory(categoryOptions) {
+        let elements = [];
+        for(let i = 0; i < categoryOptions.length; i++) {
+            elements.push(
+                <div className="checkbox" key={i}>
+                    <Checkbox value={i} key={i}>
+                        {categoryOptions[i]}
+                    </Checkbox>
+                </div>
+            );
+        }
+
+        return(
+            <CheckboxGroup onChange={this.onChangeCategory.bind(this)}>
+                {elements}
+            </CheckboxGroup>
+        );
+    }
+
+    enableSkillsetEntries() {
+        this.setState({
+            skillsetShowAllEntries: !this.state.skillsetShowAllEntries
+        });
+    }
+
+    enableCategoryEntries() {
+        this.setState({
+            categoryShowAllEntries: !this.state.categoryShowAllEntries
+        });
+    }
+
     ord_renderContent () {
+        const lookingForOptions = ['Project', 'Team'];
+        const skillsetOptions = ['C++', 'Javascript', 'Go', 'Python', 'Java'];
+        const categoryOptions = ['Social', 'IoT', 'Media', 'Finance'];
+
+        const lookingForElement = this.renderLookingFor(lookingForOptions);
+        const skillsetElement = this.renderSkillset(skillsetOptions);
+        const categoryElement = this.renderCategory(categoryOptions);
+
         return (
             <div className="p_DeveloperSearch">
                 <div className="ebp-header-divider">
@@ -45,7 +142,47 @@ export default class extends StandardPage {
                     </Row>
                     <Row className="d_row">
                         <Col span={4} className="admin-left-column wrap-box-user">
-                            FOO
+                            <Search
+                                placeholder="input search text"
+                                onSearch={value => console.log(value)}
+                                style={{ width: 200 }}/>
+
+                            <div className="group">
+                                <div className="title">Looking For:</div>
+                                <div className="content">
+                                    {lookingForElement}
+                                </div>
+                            </div>
+
+                            <div className="group">
+                                <div className="title">Skillset:</div>
+                                <div className="content">
+                                    {skillsetElement}
+                                    {skillsetOptions.length > this.state.entryCount &&
+                                        <div className="showMore" onClick={this.enableSkillsetEntries.bind(this)}>
+                                            {
+                                                !this.state.skillsetShowAllEntries ? (<span>Show More..</span>)
+                                                    : (<span>Hide</span>)
+                                            }
+                                        </div>
+                                    }
+                                </div>
+                            </div>
+
+                            <div className="group">
+                                <div className="title">Category:</div>
+                                <div className="content">
+                                    {categoryElement}
+                                    { categoryOptions.length > this.state.entryCount &&
+                                        <div className="showMore" onClick={this.enableCategoryEntries.bind(this)}>
+                                            {
+                                                !this.state.categoryShowAllEntries ? (<span>Show More..</span>)
+                                                    : (<span>Hide</span>)
+                                            }
+                                        </div>
+                                    }
+                                </div>
+                            </div>
                         </Col>
                         <Col span={20} className="admin-right-column wrap-box-user">
                             <div className="pull-right hide">
