c1fa85ba2b7354f54875ed212046b4b993ef3395
Semir Ramovic <semiir.ramovic@gmail.com>
2018-08-09 19:42:18 +0200

    Adjust layout for spinner

--- ElastosBountyProgram/front-end/src/module/project/detail/Component.js
+++ ElastosBountyProgram/front-end/src/module/project/detail/Component.js
@@ -50,51 +50,55 @@ export default class extends BaseComponent {
 
     ord_render () {
         const details = this.props.detail;
-        if(_.isEmpty(this.props.detail)) {
-            return <Spin />
-        }
+        const loading = _.isEmpty(this.props.detail)
+
         console.log(details);
         return (
             <div className="c_Project">
-                <Row className="top-section">
-                    <Col xs={24} sm={24} md={8} className="col-left">
-                        {this.renderUpperLeftBox()}
-                    </Col>
+                { loading && <Spin className="loading-spinner" />}
+                { !loading &&
+                    <div>
+                        <Row className="top-section">
+                            <Col xs={24} sm={24} md={8} className="col-left">
+                                {this.renderUpperLeftBox()}
+                            </Col>
 
-                    <Col xs={24} sm={24} md={16} className="col-right">
-                        <div className="title">
-                            <span>iOS Wallet App</span>
-                            <Icon className="badge" type="home"/>
-                        </div>
-                        <div className="leader">
-                            <Avatar size="64" style={{ backgroundColor: '#87d068' }} icon="user" />
-                            <div>Leader: Jim Bean</div>
-                        </div>
-                        <div className="content">
-                            <div className="entry">Deadline: 30 August 2018</div>
-                            <div className="entry">Progress. 70%</div>
-                            <div className="entry">Team Size: 35</div>
-                            <div className="reward">430 ELA</div>
-                        </div>
-                        <hr className="divider"/>
-                        <div className="description-title">Description</div>
-                        <hr className="divider"/>
-                        <div className="description-content">We are making the best damn thing in the world</div>
-                    </Col>
-                </Row>
-                <Row className="actions">
-                    <span className="callToActionText">Currently Hiring!</span>
-                    <Button className="colored-bottom">Join Project</Button>
-                    <Button className="normal-bottom">Message</Button>
-                    <Button className="normal-bottom">Submit Bug</Button>
-                </Row>
-                <Row className="contributors">
-                    <div>Current Contributors</div>
+                            <Col xs={24} sm={24} md={16} className="col-right">
+                                <div className="title">
+                                    <span>iOS Wallet App</span>
+                                    <Icon className="badge" type="home"/>
+                                </div>
+                                <div className="leader">
+                                    <Avatar size="64" style={{ backgroundColor: '#87d068' }} icon="user" />
+                                    <div>Leader: Jim Bean</div>
+                                </div>
+                                <div className="content">
+                                    <div className="entry">Deadline: 30 August 2018</div>
+                                    <div className="entry">Progress. 70%</div>
+                                    <div className="entry">Team Size: 35</div>
+                                    <div className="reward">430 ELA</div>
+                                </div>
+                                <hr className="divider"/>
+                                <div className="description-title">Description</div>
+                                <hr className="divider"/>
+                                <div className="description-content">We are making the best damn thing in the world</div>
+                            </Col>
+                        </Row>
+                        <Row className="actions">
+                            <span className="callToActionText">Currently Hiring!</span>
+                            <Button className="colored-bottom">Join Project</Button>
+                            <Button className="normal-bottom">Message</Button>
+                            <Button className="normal-bottom">Submit Bug</Button>
+                        </Row>
+                        <Row className="contributors">
+                            <div>Current Contributors</div>
 
-                </Row>
-                <Row className="applications">
-                    <div>Pending Applications</div>
-                </Row>
+                        </Row>
+                        <Row className="applications">
+                            <div>Pending Applications</div>
+                        </Row>
+                    </div>
+                }
             </div>
         )
     }
