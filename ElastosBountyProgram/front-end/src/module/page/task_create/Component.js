0bb7df6aeb50180899e54535ce53e1c6cdc33c8c
Kevin Tajeran <kevin.tajeran@gmail.com>
2018-08-08 18:45:59 -0700

    Updated create task page to conform to layout standards.

--- ElastosBountyProgram/front-end/src/module/page/task_create/Component.js
+++ ElastosBountyProgram/front-end/src/module/page/task_create/Component.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import StandardPage from '../StandardPage';
-import Footer from '@/module/layout/Footer/Container'
+import Navigator from '@/module/page/shared/HomeNavigator/Container'
 import TaskCreateForm from '@/module/form/TaskCreateForm/Container'
 
+import { Col, Row, Divider } from 'antd'
 import {TASK_TYPE, TASK_CATEGORY} from '@/constant'
 
 export default class extends StandardPage {
@@ -26,23 +27,35 @@ export default class extends StandardPage {
         }
 
         return (
-            <div className="p_Social">
+            <div className="c_ProfileContainer">
                 <div className="ebp-header-divider">
 
                 </div>
-                <div className="ebp-page-title">
-                    <h2>
-                        {
-                            taskType === TASK_TYPE.PROJECT
-                                ? 'Create Project'
-                                : 'Create Task / Event'
-                        }
-                    </h2>
+                <div className="p_admin_index ebp-wrap">
+                    <div className="d_box">
+                        <div className="p_admin_breadcrumb">
+                            <br/>
+                        </div>
+                        <div className="p_ProfileTeams p_admin_content">
+                            <Row>
+                                <Col span={4} className="admin-left-column wrap-box-navigator">
+                                    <Navigator selectedItem={'profileTasks'}/>
+                                </Col>
+                                <Col span={20} className="admin-right-column wrap-box-user">
+
+                                    <Divider className="">
+                                        {
+                                            taskType === TASK_TYPE.PROJECT
+                                            ? 'Create Project'
+                                            : 'Create Task / Event'
+                                        }
+                                    </Divider>
+                                    <TaskCreateForm taskType={taskType} taskCategory={taskCategory}/>
+                                </Col>
+                            </Row>
+                        </div>
+                    </div>
                 </div>
-                <div className="ebp-page">
-                    <TaskCreateForm taskType={taskType} taskCategory={taskCategory}/>
-                </div>
-                <Footer />
             </div>
         )
     }
