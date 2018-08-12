c94a93e76af730d2b6a94c66f0edd7be88c1d025
Kevin Tajeran <kevin.tajeran@gmail.com>
2018-08-12 14:57:18 -0700

    Updated and refined mobile styling for developer page.

--- ElastosBountyProgram/front-end/src/module/page/developer/Component.js
+++ ElastosBountyProgram/front-end/src/module/page/developer/Component.js
@@ -4,7 +4,9 @@ import Footer from '@/module/layout/Footer/Container'
 import I18N from '@/I18N'
 import { Link } from 'react-router-dom'
 import './style.scss'
+import MediaQuery from 'react-responsive'
 import { Col, Row, Card, Button, Breadcrumb, Icon } from 'antd'
+import {MAX_WIDTH_MOBILE} from "../../../config/constant"
 
 export default class extends StandardPage {
     navigateToLearn() {
@@ -19,11 +21,135 @@ export default class extends StandardPage {
         this.props.history.push('/developer/search?type=PROJECT');
     }
 
+    buildTopRow () {
+        return (
+            <Row className="d_row d_rowTop" type="flex" justify="center">
+                <Col sm={{span: 24}} md={{span: 8}} className="d_box" onClick={() => this.navigateToLearn()}>
+                    <Card hoverable className="feature-box">
+                        <div className="title">
+                            <span>{I18N.get('developer.learn')}</span>
+                            <img src="/assets/images/mortarboard.svg"/>
+                        </div>
+                        <hr className="feature-box-divider"/>
+                        <div className="content">
+                            <div>- {I18N.get('developer.learn.basics')}</div>
+                            <div>- {I18N.get('developer.learn.concepts')}</div>
+                            <div>- {I18N.get('developer.learn.start')}</div>
+                            <div>- {I18N.get('developer.learn.tutorials')}</div>
+                            <div>- {I18N.get('developer.learn.resources')}</div>
+                        </div>
+                    </Card>
+                </Col>
+                <Col sm={{span: 24}} md={{span: 8}} className="d_box" onClick={() => this.navigateToTeamSearch()}>
+                    <Card hoverable className="feature-box">
+                        <div className="title">
+                            <span>{I18N.get('developer.team')}</span>
+                            <img src="/assets/images/connection.svg"/>
+                        </div>
+                        <hr className="feature-box-divider"/>
+                        <div className="content">
+                            <div>- {I18N.get('developer.team.join')}</div>
+                            <div>- {I18N.get('developer.team.create_profile')}</div>
+                            <div>- {I18N.get('developer.team.create_project')}</div>
+                        </div>
+                    </Card>
+                </Col>
+                <Col sm={{span: 24}} md={{span: 8}} className="d_box" onClick={() => this.navigateToProjectSearch()}>
+                    <Card hoverable className="feature-box">
+                        <div className="title">
+                            <span>{I18N.get('developer.project')}</span>
+                            <img src="/assets/images/start.svg"/>
+                        </div>
+                        <hr className="feature-box-divider"/>
+                        <div className="content">
+                            <div>- {I18N.get('developer.project.top')}</div>
+                            <div>- {I18N.get('developer.project.join')}</div>
+                            <div>- {I18N.get('developer.project.issue')}</div>
+                        </div>
+                    </Card>
+                </Col>
+            </Row>
+        );
+    }
+
+    buildMidRow() {
+        return (
+            <MediaQuery maxWidth={MAX_WIDTH_MOBILE}>
+                {(matches) => {
+                    return (
+                        <div className="d_midRow">
+                            <Button block={matches} className="info-button">
+                                {I18N.get('developer.action')}
+                            </Button>
+                        </div>
+                    )
+                }}
+            </MediaQuery>
+        );
+    }
+
+    buildBottomRow() {
+        return (
+            <MediaQuery maxWidth={MAX_WIDTH_MOBILE}>
+                {(matches) => {
+                    return (
+                        <div className="d_bottomRow">
+                            <span className="title">{I18N.get('developer.components')}</span>
+                            <Row className="component-box">
+                                <Col sm={{span: 24}} md={{span: 18}} className="d_leftCol">
+                                    <div className="component-name">
+                                        {I18N.get('developer.components.core.title')} - <span className="languages">{I18N.get('developer.components.core.languages')}</span>
+                                    </div>
+                                    <hr className="component-divider"/>
+                                    <div className="component-description">
+                                        {I18N.get('developer.components.core.description')}
+                                    </div>
+                                </Col>
+                                <Col sm={{span: 24}} md={{span: 6}}>
+                                    <div className="button-container">
+                                        <Button block={matches} className="top-button">{I18N.get('developer.components.issues')}</Button>
+                                    </div>
+                                    <div className="button-container">
+                                        <Button block={matches}>{I18N.get('developer.components.docs')}</Button>
+                                    </div>
+                                    <div className="button-container">
+                                        <Button block={matches}>{I18N.get('developer.components.info')}</Button>
+                                    </div>
+                                </Col>
+                            </Row>
+                            <Row className="component-box">
+                                <Col sm={{span: 24}} md={{span: 18}} className="d_leftCol">
+                                    <div className="component-name">
+                                        {I18N.get('developer.components.spv')} - <span className="languages">{I18N.get('developer.components.spv.languages')}</span>
+                                    </div>
+                                    <hr className="component-divider"/>
+                                    <div className="component-description">
+                                        {I18N.get('developer.components.spv.description')}
+                                    </div>
+                                </Col>
+                                <Col sm={{span: 24}} md={{span: 6}}>
+                                    <div className="button-container">
+                                        <Button block={matches} className="top-button">{I18N.get('developer.components.issues')}</Button>
+                                    </div>
+                                    <div className="button-container">
+                                        <Button block={matches}>{I18N.get('developer.components.docs')}</Button>
+                                    </div>
+                                    <div className="button-container">
+                                        <Button block={matches}>{I18N.get('developer.components.info')}</Button>
+                                    </div>
+                                </Col>
+                            </Row>
+                        </div>
+                    );
+                }}
+            </MediaQuery>
+        );
+    }
+
     ord_renderContent () {
         return (
             <div className="p_Developer">
-                <div className="ebp-header-divider">
-                </div>
+                <div className="ebp-header-divider" />
                 <Breadcrumb className="p_admin_breadcrumb">
                     <Breadcrumb.Item><Icon type="home"/></Breadcrumb.Item>
                     <Breadcrumb.Item>{I18N.get('developer.breadcrumb.developers')}</Breadcrumb.Item>
@@ -31,92 +157,9 @@ export default class extends StandardPage {
                 <div className="p_admin_index ebp-wrap">
                     <div className="d_box">
                         <div className="p_admin_content">
-                            <Row className="d_row d_rowTop" type="flex" justify="center">
-                                <Col sm={{span: 24}} md={{span: 8}} className="d_box" onClick={() => this.navigateToLearn()}>
-                                    <Card hoverable className="feature-box">
-                                        <div className="title">
-                                            <span>{I18N.get('developer.learn')}</span>
-                                            <img src="/assets/images/mortarboard.svg"/>
-                                        </div>
-                                        <hr className="feature-box-divider"/>
-                                        <div className="content">
-                                            <div>- {I18N.get('developer.learn.basics')}</div>
-                                            <div>- {I18N.get('developer.learn.concepts')}</div>
-                                            <div>- {I18N.get('developer.learn.start')}</div>
-                                            <div>- {I18N.get('developer.learn.tutorials')}</div>
-                                            <div>- {I18N.get('developer.learn.resources')}</div>
-                                        </div>
-                                    </Card>
-                                </Col>
-                                <Col sm={{span: 24}} md={{span: 8}} className="d_box" onClick={() => this.navigateToTeamSearch()}>
-                                    <Card hoverable className="feature-box">
-                                        <div className="title">
-                                            <span>{I18N.get('developer.team')}</span>
-                                            <img src="/assets/images/connection.svg"/>
-                                        </div>
-                                        <hr className="feature-box-divider"/>
-                                        <div className="content">
-                                            <div>- {I18N.get('developer.team.join')}</div>
-                                            <div>- {I18N.get('developer.team.create_profile')}</div>
-                                            <div>- {I18N.get('developer.team.create_project')}</div>
-                                        </div>
-                                    </Card>
-                                </Col>
-                                <Col sm={{span: 24}} md={{span: 8}} className="d_box" onClick={() => this.navigateToProjectSearch()}>
-                                    <Card hoverable className="feature-box">
-                                        <div className="title">
-                                            <span>{I18N.get('developer.project')}</span>
-                                            <img src="/assets/images/start.svg"/>
-                                        </div>
-                                        <hr className="feature-box-divider"/>
-                                        <div className="content">
-                                            <div>- {I18N.get('developer.project.top')}</div>
-                                            <div>- {I18N.get('developer.project.join')}</div>
-                                            <div>- {I18N.get('developer.project.issue')}</div>
-                                        </div>
-                                    </Card>
-                                </Col>
-                            </Row>
-                            <div className="d_midRow">
-                                <Button className="info-button">
-                                    {I18N.get('developer.action')}
-                                </Button>
-                            </div>
-                            <div className="d_bottomRow">
-                                <span className="title">{I18N.get('developer.components')}</span>
-                                <Row className="component-box">
-                                    <Col sm={{span: 24}} md={{span: 18}} className="d_leftCol">
-                                        <div className="component-name">
-                                            {I18N.get('developer.components.core.title')} - <span className="languages">{I18N.get('developer.components.core.languages')}</span>
-                                        </div>
-                                        <hr className="component-divider"/>
-                                        <div className="component-description">
-                                            {I18N.get('developer.components.core.description')}
-                                        </div>
-                                    </Col>
-                                    <Col sm={{span: 24}} md={{span: 6}}>
-                                        <div className="button-container"><Button className="top-button">{I18N.get('developer.components.issues')}</Button></div>
-                                        <div className="button-container"><Button>{I18N.get('developer.components.docs')}</Button></div>
-                                        <div className="button-container"><Button>{I18N.get('developer.components.info')}</Button></div>
-                                    </Col>
-                                </Row>
-                                <Row className="component-box">
-                                    <Col sm={{span: 24}} md={{span: 18}} className="d_leftCol">
-                                        <div className="component-name">
-                                            {I18N.get('developer.components.spv')} - <span className="languages">{I18N.get('developer.components.spv.languages')}</span>
-                                        </div>
-                                        <hr className="component-divider"/>
-                                        <div className="component-description">
-                                            {I18N.get('developer.components.spv.description')}
-                                        </div>
-                                    </Col>
-                                    <Col sm={{span: 24}} md={{span: 6}}>
-                                        <div className="button-container"><Button className="top-button">{I18N.get('developer.components.issues')}</Button></div>
-                                        <div className="button-container"><Button>{I18N.get('developer.components.docs')}</Button></div>
-                                        <div className="button-container"><Button>{I18N.get('developer.components.info')}</Button></div>
-                                    </Col>
-                                </Row>
-                            </div>
+                            {this.buildTopRow()}
+                            {this.buildMidRow()}
+                            {this.buildBottomRow()}
                         </div>
                     </div>
                 </div>
