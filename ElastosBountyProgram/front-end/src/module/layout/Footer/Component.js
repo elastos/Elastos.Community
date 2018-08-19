7ad3a8258a6a006f96a3f9456c0aafd779a76322
Wiktor Starczewski <wiktor@squirro.com>
2018-08-19 22:00:18 +0200

    Change the footer

--- ElastosBountyProgram/front-end/src/module/layout/Footer/Component.js
+++ ElastosBountyProgram/front-end/src/module/layout/Footer/Component.js
@@ -8,25 +8,45 @@ export default class extends BaseComponent {
     ord_render() {
         return (
             <div className="c_Footer">
-                <div className="horizGap d_rowGrey">
+                <div className="horizGap">
 
                 </div>
-                <div className="d_rowGrey">
-                    <Row className="d_rowFooter">
-                        <Col md={{span:24}} md={{offset: 16, span: 8}}>
-                            <div className="d_footerSection">
-                                <h4>Support</h4>
-
-                                <p>
-                                    If you're a developer and are running into issues
-                                    with any of our open source projects our support team
-                                    is always here to help you
-                                    <br/>
-                                    <br/>
-                                    <a href="mailto:cyberrepublic@elastos.org">cyberrepublic@elastos.org</a>
-                                    <br/>
-                                    v0.1.0 (beta) - property of Elastos.org
-                                </p>
+                <div className="">
+                    <Row className="d_rowFooter d_footerSection">
+                        <Col span={6}>
+                            <img className="logo_own" src="/assets/images/logo_own.png"/>
+                        </Col>
+                        <Col span={4}>
+                            <div className="links footer-vertical-section">
+                                <div className="title brand-color">
+                                    Links
+                                </div>
+                                <div><a>Resources</a></div>
+                                <div><a>Resources</a></div>
+                                <div><a>Resources</a></div>
+                                <div><a>Resources</a></div>
+                            </div>
+                        </Col>
+                        <Col span={8}>
+                            <div className="contact footer-vertical-section">
+                                <div className="title brand-color">
+                                    Contact
+                                </div>
+                                <div><a>cyberrepublic@elastos.org</a></div>
+                                <div><a>cyberrepublic@elastos.org</a></div>
+                                <div><a>cyberrepublic@elastos.org</a></div>
+                                <div><a>cyberrepublic@elastos.org</a></div>
+                            </div>
+                        </Col>
+                        <Col span={6}>
+                            <div className="join footer-vertical-section">
+                                <div className="title brand-color">
+                                    Join Us On
+                                </div>
+                                <a>T</a>
+                                <a>G</a>
+                                <a>I</a>
+                                <a>R</a>
                             </div>
                         </Col>
                     </Row>
