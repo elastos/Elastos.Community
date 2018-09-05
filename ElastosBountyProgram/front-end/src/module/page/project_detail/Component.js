40d5c808af7d83a3a2603c9f54c2734a890bffdf
Wiktor Starczewski <wiktor@squirro.com>
2018-09-05 14:17:06 +0200

    Breadcrumb for cr100 detail

--- ElastosBountyProgram/front-end/src/module/page/project_detail/Component.js
+++ ElastosBountyProgram/front-end/src/module/page/project_detail/Component.js
@@ -5,12 +5,28 @@ import ProjectDetail from './detail/Container'
 import I18N from '@/I18N'
 import './style.scss'
 import _ from 'lodash'
+import { Row, Breadcrumb, Icon } from 'antd'
 
 export default class extends StandardPage {
     ord_renderContent () {
         return (
             <div className="p_ProjectDetail">
                 <div className="ebp-header-divider" />
+                <Row className="d_row d_rowTop">
+                    <div className="p_admin_breadcrumb">
+                        <Breadcrumb>
+                            <Breadcrumb.Item href="/">
+                                <Icon type="home"/>
+                            </Breadcrumb.Item>
+                            <Breadcrumb.Item href="/cr100">
+                                CR100
+                            </Breadcrumb.Item>
+                            <Breadcrumb.Item>
+                                Project Detail
+                            </Breadcrumb.Item>
+                        </Breadcrumb>
+                    </div>
+                </Row>
                 <div className="p_admin_index ebp-wrap">
                     <div className="d_box">
                         <div className="p_admin_content">
