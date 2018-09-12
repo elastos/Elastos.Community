355d3a1802537bd1c61aaa73a6e1456b874d9ad0
Wiktor Starczewski <wiktor@squirro.com>
2018-09-12 14:30:00 +0200

    fixup! Improve language switcher

--- ElastosBountyProgram/front-end/src/service/LanguageService.js
+++ ElastosBountyProgram/front-end/src/service/LanguageService.js
@@ -5,12 +5,10 @@ import I18N from '@/I18N'
 export default class extends BaseService {
 
     changeLanguage(lang) {
-
         I18N.setLang(lang);
 
         const languageRedux = this.store.getRedux('language')
+        localStorage.setItem('lang', lang)
         this.dispatch(languageRedux.actions.language_update(lang))
-
     }
-
 }
