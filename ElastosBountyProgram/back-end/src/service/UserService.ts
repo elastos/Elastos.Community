1b3d3e64fdd350845b5523010b9f0a3ab7ddb557
andy <andy.lodestar@gmail.com>
2018-09-25 20:08:51 +0800

    Fix role undefined

--- ElastosBountyProgram/back-end/src/service/UserService.ts
+++ ElastosBountyProgram/back-end/src/service/UserService.ts
@@ -132,9 +132,8 @@ export default class extends Base {
 
         let user = await db_user.findById(userId)
         let countryChanged = false
-        let is_admin = this.currentUser.role === constant.USER_ROLE.ADMIN ? true : false
 
-        if (!this.currentUser || (!is_admin && this.currentUser._id.toString() !== userId)) {
+        if (!this.currentUser || (this.currentUser.role !== constant.USER_ROLE.ADMIN && this.currentUser._id.toString() !== userId)) {
             throw 'Access Denied'
         }
 
@@ -142,7 +141,7 @@ export default class extends Base {
             throw `userId: ${userId} not found`
         }
 
-        if(is_admin){
+        if(this.currentUser.role === constant.USER_ROLE.ADMIN && param.role){
             if(Object.keys(constant.USER_ROLE).indexOf(param.role) === -1){
                 throw 'invalid role'
             }
