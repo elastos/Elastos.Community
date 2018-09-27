74db81993d6139dbbccd270ebf89e8d2c16aeac0
Semir Ramovic <semiir.ramovic@gmail.com>
2018-09-27 17:40:25 +0200

    Temp fix welcome screen

--- ElastosBountyProgram/front-end/src/module/form/LoginOrRegisterForm/Component.js
+++ ElastosBountyProgram/front-end/src/module/form/LoginOrRegisterForm/Component.js
@@ -3,7 +3,7 @@ import BaseComponent from '@/model/BaseComponent'
 import LoginForm from '@/module/form/LoginForm/Container'
 import RegisterForm from '@/module/form/RegisterForm/Container'
 import I18N from '@/I18N'
-import { Tabs } from 'antd'
+import {Tabs, Button} from 'antd'
 
 import './style.scss'
 
@@ -25,31 +25,53 @@ export default class extends BaseComponent {
         }
     }
 
+    handleSubmit(e) {
+        e.preventDefault()
+        const registerRedirect = sessionStorage.getItem('registerRedirect')
+        sessionStorage.removeItem('registerRedirect')
+        sessionStorage.removeItem('registerWelcome')
+        this.props.history.push(registerRedirect)
+    }
+
+    showPostRegLogScreen() {
+        return (
+            <div className="post-state">
+                <h3 className="welcome-header komu-a">{I18N.get('register.welcome')}</h3>
+                <p className="welcome-text synthese">{I18N.get('register.join_circle')}</p>
+                <Button type="ebp" htmlType="submit" className="d_btn d_btn_join" onClick={this.handleSubmit.bind(this)}>
+                    {I18N.get('register.join')}
+                </Button>
+            </div>
+        )
+    }
+
     ord_render() {
         const registered = sessionStorage.getItem('registered')
 
-        if (registered) {
+        if (registered === '1') {
             this.setState({
                 activeKey: 'login'
             })
             sessionStorage.removeItem('registered')
-            console.log("removed registered")
         }
 
+        const welcomeState = sessionStorage.getItem('registerWelcome') === '1'
+        console.log(sessionStorage)
         return (
             <div className="c_LoginOrRegister">
                 <div className="pull-left">
                     <img src="/assets/images/login-left.png"/>
                 </div>
                 <div className="main-form">
-                    <Tabs activeKey={this.state.activeKey} onChange={this.handleChangeTab()}>
-                        <TabPane tab="Login" key="login">
-                            <LoginForm />
-                        </TabPane>
-                        <TabPane tab="Register" key="register">
-                            <RegisterForm />
-                        </TabPane>
-                    </Tabs>
+                    {this.state.activeKey === 'post' ? this.showPostRegLogScreen() : (
+                        <Tabs activeKey={this.state.activeKey} onChange={this.handleChangeTab()}>
+                            <TabPane tab="Login" key="login">
+                                <LoginForm />
+                            </TabPane>
+                            <TabPane tab="Register" key="register">
+                                <RegisterForm onChangeActiveKey={this.handleChangeTab()}/>
+                            </TabPane>
+                        </Tabs>)}
                 </div>
             </div>
         )
