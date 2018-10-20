b977c272dc8a618f4cad90566afa5b774005e267
Semir Ramovic <semiir.ramovic@gmail.com>
2018-10-20 21:30:28 +0200

    Redesign of task application view

--- ElastosBountyProgram/front-end/src/module/project/application/Component.js
+++ ElastosBountyProgram/front-end/src/module/project/application/Component.js
@@ -2,12 +2,14 @@ import React from 'react';
 import BaseComponent from '@/model/BaseComponent'
 import moment from 'moment'
 import _ from 'lodash'
-import { Col, Row, Button, Spin, Divider, message, List, Icon, Tooltip, Popconfirm, Card, Avatar, InputNumber } from 'antd'
+import { Col, Row, message, Input, Avatar, InputNumber, Select } from 'antd'
 import {TASK_CATEGORY, TASK_TYPE, TASK_STATUS, TASK_CANDIDATE_TYPE, TASK_CANDIDATE_STATUS} from '@/constant'
 import Comments from '@/module/common/comments/Container'
 import './style.scss'
 import I18N from '@/I18N'
 
+const { TextArea } = Input;
+const Option = Select.Option;
 const dateTimeFormat = 'MMM D, YYYY - h:mma (Z [GMT])'
 
 export default class extends BaseComponent {
@@ -33,59 +35,55 @@ export default class extends BaseComponent {
         if (!applicant) {
             return ''
         }
-        const appliedDate = moment(applicant.createdAt).format('MMM D, YYYY')
+        const appliedDate = moment(applicant.createdAt).format('YYYY / MM / D')
 
         return (
             <div className="public">
-                <Row>
-                    <Col span={24} className="gridCol main-area">
-                        <Row>
-                            <Col>
-                                <Card title={`Applied on ${appliedDate}`} extra={
-                                    <div>
-
+                <div className="gridCol main-area">
+                    <div className="application-col">
+                        <h3 className="header-text with-gizmo">Application</h3>
+                        <div className="application-container">Applied on
+                            <span className="application-date"> {appliedDate}</span>
+                        </div>
+                        <div>
+                            <Select
+                                className="apply-type-select cr-input"
+                                defaultValue="solo">
+                                <Option value="solo"><span>Apply as an Individual</span> (Solo)</Option>
+                                <Option value="team"><span>Apply as a Team</span> (Team)</Option>
+                            </Select>
+                        </div>
+                        { this.props.detail.bidding &&
+                            <div className="bid-container">
+                                { !this.isTaskOwner() && _.indexOf(['PENDING', 'CREATED'], this.props.detail.status) >= 0 && (applicant.type !== TASK_CANDIDATE_TYPE.TEAM || applicant.team.owner._id === this.props.userId)
+                                    ? <div>
+                                        <span>{I18N.get('project.detail.your_bid')} </span>
+                                        <InputNumber onChange={_.debounce((value) => this.changeBid(value), 2000)}
+                                            defaultValue={applicant.bid}/>
+                                        <span> ELA</span>
                                     </div>
-                                }>
-                                    {applicant.user &&
-                                        <a onClick={this.linkUserDetail.bind(this, applicant.user)}>
-                                            <Avatar className="gap-right" src={applicant.user.profile.avatar}/>
-                                            {`${applicant.user.profile.firstName} ${applicant.user.profile.lastName}`}
-                                        </a>
-                                    }
-                                    <h5>
-                                        {applicant.applyMsg}
-                                    </h5>
-                                    { this.props.detail.bidding &&
-                                        <div>
-                                            { !this.isTaskOwner() && _.indexOf(['PENDING', 'CREATED'], this.props.detail.status) >= 0 && (applicant.type !== TASK_CANDIDATE_TYPE.TEAM || applicant.team.owner._id === this.props.userId)
-                                                ? <div>
-                                                    <span>{I18N.get('project.detail.your_bid')}: </span>
-                                                    <InputNumber onChange={_.debounce((value) => this.changeBid(value), 2000)}
-                                                        defaultValue={applicant.bid}/>
-                                                    <span> ELA</span>
-                                                </div>
-                                                : <div>
-                                                    <h5>
-                                                        Bid: {applicant.bid} ELA
-                                                    </h5>
-                                                </div>
-                                            }
-                                        </div>
-                                    }
-                                    {
-                                        this.showAttachment()
-                                    }
-                                </Card>
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Comments type="taskCandidate" reduxType="task" canPost={true} model={applicant}
-                                detailReducer={(detail) => _.find(detail.candidates, { _id: this.props.applicantId })}
-                                returnUrl={`/project-detail/${this.props.detail._id}`}
-                            />
-                        </Row>
-                    </Col>
-                </Row>
+                                    : <div>
+                                        <h5>
+                                            Bid: {applicant.bid} ELA
+                                        </h5>
+                                    </div>
+                                }
+                            </div>
+                        }
+                        <div>
+                            <div className="task-reason">Why you wanted to join this task</div>
+                            <div className="task-reason-text-area">
+                                <p>{applicant.applyMsg}</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="comments-col">
+                        <Comments type="taskCandidate" reduxType="task" canPost={true} model={applicant}
+                            detailReducer={(detail) => _.find(detail.candidates, { _id: this.props.applicantId })}
+                            returnUrl={`/project-detail/${this.props.detail._id}`}
+                        />
+                    </div>
+                </div>
             </div>
         )
     }
