2907513037aa77dfaafa39de79d236f8683e21e0
Wiktor Starczewski <poszerny@gmail.com>
2018-10-25 15:34:53 +0200

    Propose new tasks in modal

--- ElastosBountyProgram/front-end/src/I18N/en.js
+++ ElastosBountyProgram/front-end/src/I18N/en.js
@@ -413,6 +413,7 @@ const en = {
     'task.soloApply': 'Apply as an Individual',
     'task.teamApply': 'Apply as a Team',
     'task.applyReason': 'Why you wanted to join this task',
+    'task.createNew': 'Propose new Task',
 
     // General
     'select.placeholder': 'Please select',
