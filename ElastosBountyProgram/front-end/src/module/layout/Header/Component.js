953904e219491b6bc712bf3b8bbc3d6700a73f0f
Clarence Liu <clarence.hl.liu@gmail.com>
2018-10-28 20:15:18 -0700

    missing community header link

--- ElastosBountyProgram/front-end/src/module/layout/Header/Component.js
+++ ElastosBountyProgram/front-end/src/module/layout/Header/Component.js
@@ -204,6 +204,10 @@ export default class extends BaseComponent {
                         {I18N.get('0106')}
                     </Menu.Item>
 
+                    <Menu.Item className="c_MenuItem link" key="developer">
+                        {I18N.get('0102')}
+                    </Menu.Item>
+
                     <Menu.Item className="c_MenuItem link" key="council">
                         {I18N.get('council.0001')}
                     </Menu.Item>
