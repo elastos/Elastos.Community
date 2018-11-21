046d018807957d55826e1eaea77c5b06fb9a69ed
Wiktor Starczewski <poszerny@gmail.com>
2018-11-21 23:43:02 +0100

    Fix padding on the migrated admin screens

--- ElastosBountyProgram/front-end/src/module/page/admin/community/CountryCommunities/Component.js
+++ ElastosBountyProgram/front-end/src/module/page/admin/community/CountryCommunities/Component.js
@@ -235,7 +235,7 @@ export default class extends AdminPage {
                 <div className="d_box">
                     <div className="p_admin_content">
                         <Row>
-                            <Col span={4} className="admin-left-column wrap-box-navigator">
+                            <Col span={4} className="wrap-box-navigator">
                                 <Navigator selectedItem={'community'}/>
                             </Col>
                             <Col span={20} className="admin-right-column wrap-box-user">
