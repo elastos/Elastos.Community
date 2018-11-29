08dda3b2db29df6a2a601a1ef6a126ff78c3e0bb
Jay Lord Torres <isens.jaylord@gmail.com>
2018-11-29 21:19:42 +0800

    fronend: update cvote test data

--- ElastosBountyProgram/back-end/test/unit/data/cvote.js
+++ ElastosBountyProgram/back-end/test/unit/data/cvote.js
@@ -3,8 +3,10 @@ global.DB = Object.assign(global.DB, {
 
     CVOTE_1: {
         title: 'Hello World',
+        title_zh: 'Hello World _zh',
         type: 1,
         content: 'Lorem Ipsum',
+        content_zh: 'Lorem Ipsum_zh',
         proposedBy: 'Kevin Zhang',
         isConflict: 'NO',
         status: 'ACTIVE',
